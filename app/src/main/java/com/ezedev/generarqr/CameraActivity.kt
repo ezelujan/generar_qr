07727207943f18effe297bcfa43542994f67bfe5
Ezequiel Luján Escobar <ojos1098@gmail.com>
2022-05-17 18:10:31 -0500

    feat: tomar foto

--- app/src/main/java/com/ezedev/generarqr/CameraActivity.kt
+++ app/src/main/java/com/ezedev/generarqr/CameraActivity.kt
@@ -1,27 +1,39 @@
 package com.ezedev.generarqr
 
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.ezedev.generarqr.databinding.ActivityCameraBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
     private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        
         if (allPermissionsGranted()) {
             startCamera()
         } else {
@@ -31,6 +43,47 @@ class CameraActivity : AppCompatActivity() {
                 Constants.REQUEST_CODE_PERMISSIONS
             )
         }
+
+        binding.btnTakePhoto.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let { mFile ->
+            File(mFile, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(
+                Constants.FILE_NAME_FORMAT,
+                Locale.getDefault()
+            ).format(System.currentTimeMillis()) + Constants.FILE_EXTENSION
+        )
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(this@CameraActivity, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(Constants.TAG, msg)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG, "Photo capture failed: ${exception.message}", exception)
+                }
+            }
+        )
     }
 
     private fun startCamera() {
@@ -87,4 +140,9 @@ class CameraActivity : AppCompatActivity() {
                 baseContext, it
             ) == PackageManager.PERMISSION_GRANTED
         }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
 }
--- app/src/main/java/com/ezedev/generarqr/Constants.kt
+++ app/src/main/java/com/ezedev/generarqr/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
     const val TAG = "cameraX"
     const val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
     const val REQUEST_CODE_PERMISSIONS = 123
+    const val FILE_EXTENSION = ".jpg"
     val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
 }
